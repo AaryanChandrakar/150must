618e41ef48282989b762c15e7b462a65dcba1883
Aaryan Chandrakar <146125867+AaryanChandrakar@users.noreply.github.com>
2025-03-06 17:45:58 +0000

    commit

--- Array/Hashing/Two_Sum.java
+++ Array/Hashing/Two_Sum.java
@@ -1,6 +1,7 @@
-public class Two_Sum {
+import java.util.*;
+class Two_Sum {
 
-    public int[] twoSum(int[] nums, int target) 
+    public static int[] twoSum(int[] nums, int target) 
     {
                           //T(n)=n^2
         int n=nums.length;
@@ -27,9 +28,9 @@ public class Two_Sum {
     }
     public static void main(String[] args)
     {
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        twoSum(nums, target);
+        int[] nums = {7,5,3,5,8};
+        int target = 13;
+        System.out.print(Arrays.toString(twoSum(nums, target)));
 
     }
 }
---
+++ Array/Hashing/demo.java
@@ -0,0 +1,13 @@
+
+
+class Demo {
+    int x = 5;
+
+    public static void main(String arg[])
+    {
+        Demo d = new Demo();
+        System.out.println(d.x);
+
+    }
+
+}
